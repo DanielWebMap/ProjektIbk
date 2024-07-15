d318813b639e3d1c23b14029c9484b52b4256975
DanielWebMap <Daniel.schoenung@student.uibk.ac.at>
2024-07-15 09:36:30 -0700

    150724

--- Wanderungen.js
+++ Wanderungen.js
@@ -25,3 +25,45 @@ let controlElevation = L.control.elevation({
     theme: "ibk_hiking",
 }).addTo(w_map);
 controlElevation.load("Wanderungen/Seefeld.gpx");
+
+
+//SLIDESHOW
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Vorwärts/Rückwärts Steuerung
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Aktuelles Bild anzeigen
+function showSlides(n) {
+    let i;
+    
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+
+
+//FUNKTION FÜR ÄNDERUNG DES INHALTS
+
+function InhaltUmschalten() {
+
+    let content1 = document.getElementById("Seefeld");
+    let content2 = document.getElementById("Achensee");
+
+    if (content1.style.display === "none") {
+        content1.style.display = "block";
+        content2.style.display = "none";
+    } else {
+        content1.style.display = "none";
+        content2.style.display = "block";
+    }
+}
