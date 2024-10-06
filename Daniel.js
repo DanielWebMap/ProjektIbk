bf477d6a2b992f4ad24b54fab1d0b0948ed69eb8
DanielWebMap <Daniel.schoenung@student.uibk.ac.at>
2024-10-06 19:45:59 +0200

    Update

--- Daniel.js
+++ Daniel.js
@@ -11,11 +11,8 @@ else {
 }
 
 
-var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 0,
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'png'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(D_map);
 
 L.control.scale({
