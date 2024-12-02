ae316f53828e2541cda16456a9a5e4109f295192
danielwebmap <Daniel.schoenung@student.uibk.ac.at>
2024-12-02 18:23:55 +0100

    Änderungen Diagramm

--- main.js
+++ main.js
@@ -9,23 +9,28 @@ menuToggle.addEventListener('click', function() {
 
 //CHARTS DIAGRAMM
 
+var windowWidth = $(window).width();
+if (windowWidth > 400) {
+
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ['Aktivität', 'Anzahl'],
     ['Skitage',3],
-    ['Wanderungen', 8],
-    ['Fahrradtouren',5],
-    ['Rodeln',1],
+    ['Wanderungen', 12],
+    ['Fahrradtouren',7],
+    ['Rodeltage',1],
+    ['Kurztrips',2]
   ]);
+
 
   var options = {
         pieHole: 0.3,
-        slices: { 0: { color: '#2e9eda' }, 1: { color: '#9eda2e' }, 2: { color: '#5fbcc3' }, 3: { color: '#2a6ac3' }, 4: { color: '#D0D07B' }, 5: { color: '#DE7080' }, 6: { color: '#B374CA' } },
+        slices: { 0: { color: '#2e9eda' }, 1: { color: '#9eda2e' }, 2: { color: '#5fbcc3' }, 3: { color: '#2a6ac3' }, 4: { color: '#D0D07B' }, 5: { color: '#DE7080' }, 6: { color: '#B374CA' }, },
         backgroundColor: 'none',
-        width: "800",
-        height: "350",
+        width: 800,
+        height: 350,
         legend: { position: 'right' },
         pieSliceTextStyle: {
             color: 'black',
@@ -34,6 +39,43 @@ function drawChart() {
 
   var chart = new google.visualization.PieChart(document.getElementById('chart'));
   chart.draw(data, options);
+  
+  
 }
+}
+
+else if (windowWidth < 400) {
+
+
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Aktivität', 'Anzahl'],
+      ['Skitage',3],
+      ['Wanderungen', 12],
+      ['Fahrradtouren',7],
+      ['Rodeltage',1],
+      ['Kurztrips',2]
+    ]);
+  
+  
+    var options = {
+          pieHole: 0.3,
+          slices: { 0: { color: '#2e9eda' }, 1: { color: '#9eda2e' }, 2: { color: '#5fbcc3' }, 3: { color: '#2a6ac3' }, 4: { color: '#D0D07B' }, 5: { color: '#DE7080' }, 6: { color: '#B374CA' }, },
+          backgroundColor: 'none',
+          width: 380,
+          height: 250,
+          legend: { position: 'right', textStyle: {
+            fontSize: 9 } },
+          pieSliceTextStyle: {
+              color: 'black',
+          }
+    };
+  
+    var chart = new google.visualization.PieChart(document.getElementById('chart'));
+    chart.draw(data, options);
+  }
+  }
 
 
